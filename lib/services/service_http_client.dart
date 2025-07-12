1d3a92143eeb6605055053a8a286f1cf40d78ea5
bimbim1214 <bimoadityapangestu@gmail.com>
2025-07-12 23:39:38 +0700

    Tambah metode HTTP PUT dan DELETE dengan token

--- lib/services/service_http_client.dart
+++ lib/services/service_http_client.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart' as secureStorage;
 
 class ServiceHttpClient {
-  final String baseUrl = 'http://192.168.22.188:8000/api/';
+  final String baseUrl = 'http://192.168.1.7:8000/api/';
   final secureStorage = FlutterSecureStorage();
 
   //post
@@ -66,6 +68,44 @@ class ServiceHttpClient {
       return response;
     } catch (e) {
       throw Exception('Get request failed: $e');
+    }
+  }
+  // PUT with token
+  Future<http.Response> put(String endpoint, Map<String, dynamic> body) async {
+    final token = await secureStorage.read(key: 'authToken');
+    final url = Uri.parse('$baseUrl$endpoint');
+    try {
+      final response = await http.put(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+        body: json.encode(body),
+      );
+      return response;
+    } catch (e) {
+      throw Exception('Put request failed: $e');
+    }
+  }
+
+  // DELETE with token
+  Future<http.Response> delete(String endpoint) async {
+    final token = await secureStorage.read(key: 'authToken');
+    final url = Uri.parse('$baseUrl$endpoint');
+    try {
+      final response = await http.delete(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+      );
+      return response;
+    } catch (e) {
+      throw Exception('Delete request failed: $e');
     }
   }
 }
