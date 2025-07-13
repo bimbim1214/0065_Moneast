6ac157db49af503ee6d96ba57878964b94d002f0
bimbim1214 <bimoadityapangestu@gmail.com>
2025-07-13 23:35:17 +0700

    menambahkan bocprovider di main

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pamfix/data/repository/auth_repository.dart';
+import 'package:pamfix/data/repository/buah_repository.dart';
+import 'package:pamfix/data/repository/kategori_buah_repository.dart';
+import 'package:pamfix/data/repository/pembelian_repository.dart';
+import 'package:pamfix/data/repository/supplier_repository.dart';
+import 'package:pamfix/presentation/auth/bloc/kategori/kategori_bloc.dart';
 import 'package:pamfix/presentation/auth/bloc/login/login_bloc.dart';
+import 'package:pamfix/presentation/auth/bloc/pembelian/pembelian_bloc.dart';
 import 'package:pamfix/presentation/auth/bloc/register/register_bloc.dart';
+import 'package:pamfix/presentation/auth/bloc/supplier/supplier_bloc.dart';
+import 'package:pamfix/presentation/auth/buah_screen.dart';
+import 'package:pamfix/presentation/auth/kategori_screen.dart';
 import 'package:pamfix/presentation/auth/login_screen.dart';
+import 'package:pamfix/presentation/auth/bloc/buah/buah_bloc.dart';
+import 'package:pamfix/presentation/auth/pembelian_screen.dart';
+import 'package:pamfix/presentation/auth/supplier_screen.dart';
 import 'package:pamfix/services/service_http_client.dart';
+
+
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget { 
   const MyApp({super.key});
 
   // This widget is the root of your application.
@@ -18,7 +32,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        
         BlocProvider(
           create: (context) => 
           LoginBloc(authRepository: AuthRepository(ServiceHttpClient())),
@@ -27,13 +40,33 @@ class MyApp extends StatelessWidget {
           create: (context) => 
           RegisterBloc(authRepository: AuthRepository(ServiceHttpClient())),
         ),
+        BlocProvider(
+          create: (context) => 
+          BuahBloc(buahRepository: BuahRepository(ServiceHttpClient())),
+        ),
+        BlocProvider(
+          create: (context) => 
+          KategoriBloc(kategoriRepository: KategoriRepository(ServiceHttpClient())),
+        ),
+        BlocProvider(
+          create: (context) => 
+          SupplierBloc(supplierRepository: SupplierRepository(ServiceHttpClient())),
+        ),
+        BlocProvider(
+          create: (context) => 
+          PembelianBloc(pembelianRepository: PembelianRepository(ServiceHttpClient())),
+        ),
       ],
       child: MaterialApp(
         title: 'PamFix',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
+        // home: const BuahScreen(), 
+        // home: const KategoriScreen(),
         home: const LoginScreen(),
+        // home: const SupplierScreen(),
+        // home: const PembelianScreen(),
       ),
     );
   }
