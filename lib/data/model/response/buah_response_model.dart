f83012e81779fad9b69710aaf137bd49ab47f869
bimbim1214 <bimoadityapangestu@gmail.com>
2025-07-13 23:30:56 +0700

    menambahkan kategori buah id di buah response

--- lib/data/model/response/buah_response_model.dart
+++ lib/data/model/response/buah_response_model.dart
@@ -1,37 +1,33 @@
 import 'dart:convert';
 
-class BuahResponseModel {
+class BuahResponsesModel {
     final String? nama;
     final int? stok;
-    final DateTime? updatedAt;
-    final DateTime? createdAt;
+    final int? kategoriBuahId;
     final int? id;
 
-    BuahResponseModel({
+    BuahResponsesModel({
         this.nama,
         this.stok,
-        this.updatedAt,
-        this.createdAt,
+        this.kategoriBuahId,
         this.id,
     });
 
-    factory BuahResponseModel.fromJson(String str) => BuahResponseModel.fromMap(json.decode(str));
+    factory BuahResponsesModel.fromJson(String str) => BuahResponsesModel.fromMap(json.decode(str));
 
     String toJson() => json.encode(toMap());
 
-    factory BuahResponseModel.fromMap(Map<String, dynamic> json) => BuahResponseModel(
+    factory BuahResponsesModel.fromMap(Map<String, dynamic> json) => BuahResponsesModel(
         nama: json["nama"],
         stok: json["stok"],
-        updatedAt: json["updated_at"] == null ? null : DateTime.parse(json["updated_at"]),
-        createdAt: json["created_at"] == null ? null : DateTime.parse(json["created_at"]),
+        kategoriBuahId: json["kategori_buah_id"],
         id: json["id"],
     );
 
     Map<String, dynamic> toMap() => {
         "nama": nama,
         "stok": stok,
-        "updated_at": updatedAt?.toIso8601String(),
-        "created_at": createdAt?.toIso8601String(),
+        "kategori_buah_id": kategoriBuahId,
         "id": id,
     };
 }
