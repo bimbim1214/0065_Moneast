cbcecb292e7a5724f138484fee1658c72bcf3e53
bimbim1214 <bimoadityapangestu@gmail.com>
2025-07-13 23:30:12 +0700

    menambahkan kategoribuah id di buah request

--- lib/data/model/request/admin/buah_request_model.dart
+++ lib/data/model/request/admin/buah_request_model.dart
@@ -3,10 +3,12 @@ import 'dart:convert';
 class BuahRequestModel {
     final String? nama;
     final int? stok;
+    final int? kategoriBuahId;
 
     BuahRequestModel({
         this.nama,
         this.stok,
+        this.kategoriBuahId,
     });
 
     factory BuahRequestModel.fromJson(String str) => BuahRequestModel.fromMap(json.decode(str));
@@ -16,10 +18,12 @@ class BuahRequestModel {
     factory BuahRequestModel.fromMap(Map<String, dynamic> json) => BuahRequestModel(
         nama: json["nama"],
         stok: json["stok"],
+        kategoriBuahId: json["kategori_buah_id"],
     );
 
     Map<String, dynamic> toMap() => {
         "nama": nama,
         "stok": stok,
+        "kategori_buah_id": kategoriBuahId,
     };
 }
